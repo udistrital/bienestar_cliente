522a017a61741c96d7b76dcf697cc05041aa158e
David Morales <daamoralesm@correo.udistrital.edu.co>
2019-08-13 15:02:59 -0500

    resolve logout and install ng-bootstrap

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   exports: [
+    CommonModule,
     TranslateModule,
   ],
 })
