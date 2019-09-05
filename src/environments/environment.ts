340c53489aa13d1b80f71027d6b301c253fd17a5
Soulfilip-ud <efcanastoj@udistrital.edu.co>
2019-09-05 17:45:03 -0500

    Se agregan los nuevos token id, se cambian las url de los API, y se quita el base href ya no es necesario

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,26 +9,19 @@ export const environment = {
     PATH: 'https://documental.udistrital.edu.co/nuxeo/',
   },
   WSO2_SERVICE: 'https://autenticacion.udistrital.edu.co/apioas/',
-  PLAN_CUENTAS_CRUD_SERVICE: 'http://localhost:8080/v1/',
-  PLAN_CUENTAS_MID_SERVICE: 'https://localhost:8084/v1/',
   PLAN_CUENTAS_MONGO_SERVICE: 'http://localhost:8082/v1/',
-
-  // PLAN_CUENTAS_CRUD_SERVICE: 'https://autenticacion.udistrital.edu.co/apioas/plan_cuentas_crud/v1/',
-  // PLAN_CUENTAS_MID_SERVICE: 'https://autenticacion.udistrital.edu.co/apioas/plan_cuentas_mid/v1/',
-  // PLAN_CUENTAS_MONGO_SERVICE: 'https://autenticacion.udistrital.edu.co/apioas/plan_cuentas_mongo_crud/v1/',
-
   OIKOS_SERVICE: 'http://10.20.0.254/oikos_api/v1/',
   CONFIGURACION_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/',
   NOTIFICACION_SERVICE: 'ws://10.20.0.254/notificacionws/ws/join',
   CONF_MENU_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/menu_opcion_padre/ArbolMenus/',
   TOKEN: {
     AUTORIZATION_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize',
-    CLIENTE_ID: 'FGCTE3oTXgu5D2Y_UlBB67BGkxMa',
+    CLIENTE_ID: 'e36v1MPQk2jbz9KM4SmKhk8Cyw0a',
     RESPONSE_TYPE: 'id_token token',
     SCOPE: 'openid email role',
-    REDIRECT_URL: 'http://localhost:4200/presupuesto/',
+    REDIRECT_URL: 'http://localhost:4200/',
     SIGN_OUT_URL: 'https://autenticacion.udistrital.edu.co/oidc/logout',
-    SIGN_OUT_REDIRECT_URL: 'http://localhost:4200/presupuesto/',
+    SIGN_OUT_REDIRECT_URL: 'http://localhost:4200/',
   },
 
 };
