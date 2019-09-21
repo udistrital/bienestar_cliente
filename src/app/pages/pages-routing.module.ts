a7d8b5900ad4abb74fd7958200c5efc4e7ec2216
David Morales <daamoralesm@correo.udistrital.edu.co>
2019-09-20 19:20:39 -0500

    change logo, redirect dashboard and something labels
    se cambia logo, redireccion pagina de inicio y algunos labels

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -75,7 +75,7 @@ const routes: Routes = [{
     },
     {
       path: '',
-      redirectTo: 'dashboard',
+      redirectTo: 'plan-cuentas',
       pathMatch: 'full',
     },
     {
--- src/app/pages/plan-cuentas/plan-cuentas-routing.module.ts
+++ src/app/pages/plan-cuentas/plan-cuentas-routing.module.ts
@@ -33,6 +33,11 @@ const routes: Routes = [{
   path: 'fuentes',
   component: ListFuenteComponent,
 },
+{
+  path: '',
+  redirectTo: 'rubros',
+  pathMatch: 'full',
+}
 
 ];
 
