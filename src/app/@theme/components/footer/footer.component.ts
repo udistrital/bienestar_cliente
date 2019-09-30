5c6e14a20407b1ecfdc6c37a7c9e67e6dedc6a17
AlaskaRising <nicostevrr@gmail.com>
2019-09-30 16:16:01 -0500

    Estilos del footer #63

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -3,15 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
-  template: `
-    <span class="created-by">Creado con ♥ by <b>Oficina Asesora de Sistemas</b></span>
-    <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
-    </div>
-  `,
+  templateUrl: './footer.component.html',
 })
 export class FooterComponent {
 }
