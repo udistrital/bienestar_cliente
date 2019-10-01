ba3790fcf209f5d7c4658179399ba9eba27f1a9e
AlaskaRising <nicostevrr@gmail.com>
2019-10-01 13:05:39 -0500

    Estilos del footer #64

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'ngx-footer',
@@ -6,4 +8,41 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
 })
 export class FooterComponent {
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'clock-outline',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        'presupuesto/../assets/images/clock-outline.svg'
+      )
+    );
+    this.matIconRegistry.addSvgIcon(
+      'globe-outline',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        'presupuesto/../assets/images/globe-outline.svg'
+      )
+    );
+    this.matIconRegistry.addSvgIcon(
+      'at-outline',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        'presupuesto/../assets/images/at-outline.svg'
+      )
+    );
+    this.matIconRegistry.addSvgIcon(
+      'phone-outline',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        'presupuesto/../assets/images/phone-outline.svg'
+      )
+    );
+
+    this.matIconRegistry.addSvgIcon(
+      'pin-outline',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        'presupuesto/../assets/images/pin-outline.svg'
+      )
+    );
+      }
+
 }
