cdc12bb366fdcaebbacea40d350c0fd191f00ff5
Soulfilip <filip9322@gmail.com>
2019-10-14 01:36:41 -0500

    Se agregan las palabras para traducir en el footer, hay errores :/ no encuentra el pipeline de trasnlate

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'ngx-footer',
@@ -10,39 +11,43 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class FooterComponent {
   constructor(
     private matIconRegistry: MatIconRegistry,
+    private translate: TranslateService,
     private domSanitizer: DomSanitizer,
-  ) {
+    ) {
     this.matIconRegistry.addSvgIcon(
       'clock-outline',
       this.domSanitizer.bypassSecurityTrustResourceUrl(
         'presupuesto/../assets/images/clock-outline.svg'
-      )
-    );
+        )
+      );
     this.matIconRegistry.addSvgIcon(
       'globe-outline',
       this.domSanitizer.bypassSecurityTrustResourceUrl(
         'presupuesto/../assets/images/globe-outline.svg'
-      )
-    );
+        )
+      );
     this.matIconRegistry.addSvgIcon(
       'at-outline',
       this.domSanitizer.bypassSecurityTrustResourceUrl(
         'presupuesto/../assets/images/at-outline.svg'
-      )
-    );
+        )
+      );
     this.matIconRegistry.addSvgIcon(
       'phone-outline',
       this.domSanitizer.bypassSecurityTrustResourceUrl(
         'presupuesto/../assets/images/phone-outline.svg'
-      )
-    );
+        )
+      );
 
     this.matIconRegistry.addSvgIcon(
       'pin-outline',
       this.domSanitizer.bypassSecurityTrustResourceUrl(
         'presupuesto/../assets/images/pin-outline.svg'
-      )
-    );
-      }
-
+        )
+      );
+  }
+  ngOnInit(): void {
+    this.translate.setDefaultLang('es');
+    this.translate.use(this.translate.getBrowserLang());
+  }
 }
