51825a6cde877d12104b66f32af03fb372c4f51a
Soulfilip-ud <efcanastoj@udistrital.edu.co>
2019-10-14 23:03:47 -0500

    Listo traducciones en el footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,17 +5,6 @@ import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 @NgModule({
-  imports: [ 
-  	TranslateModule.forChild({
-  		useDefaultLang: true,
-  		isolate : true,
-      loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient]
-      }
-    })
-  ] ,
   exports: [
     CommonModule,
     TranslateModule,
