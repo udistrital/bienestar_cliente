a914b11c240631ed98506900e070b54be1d8a81d
David Morales <daamoralesm@correo.udistrital.edu.co>
2019-10-23 19:25:00 -0500

    se agrega columna de vigencia y codigo, se corrigen mensajes
    desarrollo de productos-rubros no se continua para tratar desarrollos vitales como fuentes,metas,actividades

--- src/app/@core/data/validators.service.ts
+++ src/app/@core/data/validators.service.ts
@@ -1,13 +1,14 @@
 export class ValidationService {
     static getValidatorErrorMessage(validatorName: string, validatorValue?: any) {
       let config = {
-        'required': 'Required',
+        'required': 'Valor Requerido',
         'invalidCreditCard': 'Is invalid credit card number',
         'invalidEmailAddress': 'Invalid email address',
         'invalidPassword': 'Invalid password. Password must be at least 6 characters long, and contain a number.',
-        'minlength': `Minimum length ${validatorValue.requiredLength}`
+        'minlength': `Minimum length ${validatorValue.requiredLength}`,
+        'min':  `Minimo % ${validatorValue.min}`,
+        'max':  `Maximo % ${validatorValue.max}`
       };
-  
       return config[validatorName];
     }
   
