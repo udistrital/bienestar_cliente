809355b005f4cea83773e95bd503fe674c1cd59e
Juan Camilo Sarmiento Reyes <jcsarmientor@correo.udistrital.edu.co>
2019-11-22 08:34:57 -0500

    updating environment vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -14,6 +14,8 @@ export const environment = {
   PLAN_CUENTAS_MONGO_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/plan_cuentas_mongo_crud/v1/',
   CONFIGURACION_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/',
   NOTIFICACION_SERVICE: 'wss://autenticacion.portaloas.udistrital.edu.co/apioas/notificacion_ws/v1/',
+  CORE_SERVICE: 'http://pruebasapi.intranetoas.udistrital.edu.co:8092/v1/',
+  CORE_AMAZON_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/core_amazon_crud/v1/",
   CONF_MENU_SERVICE:
   'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/menu_opcion_padre/ArbolMenus/',
   TOKEN: {
