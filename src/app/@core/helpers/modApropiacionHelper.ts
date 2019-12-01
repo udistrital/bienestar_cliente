6d9e6cb94d1a39c07763ac6cf4fbf6a96854da94
miguel.ramirez93 <miguel.ramirez@taximo.co>
2019-11-30 22:15:42 -0500

    Fix erased lines

--- src/app/@core/helpers/modApropiacionHelper.ts
+++ src/app/@core/helpers/modApropiacionHelper.ts
@@ -41,7 +41,7 @@ export class ModApropiacionHelper {
 
     public getAllModificacionesApr(id?: any, params?: {vigencia: string, cg: string}){
         this.rqManager.setPath('PLAN_CUENTAS_MONGO_SERVICE');
-        return this.rqManager.get(`documento_presupuestal/${params.vigencia}/${params.cg}/modificacion`).pipe(
+        return this.rqManager.get(`documento_presupuestal/${params.vigencia}/${params.cg}/modificacion_presupuestal`).pipe(
             map(
                 (res) => {
                     if (res['Type'] === 'error') {
