7a79f39e550c3243f102a636b97573ee3e5ee878
MVC2 <miltonv.calderonc@gmail.com>
2020-10-16 22:18:22 -0500

    navegación ok hasta lista reliquidación

---
+++ src/app/pages/reliquidacion/lista-estudiantes-reliquidacion/lista-estudiantes-reliquidacion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-lista-estudiantes-reliquidacion',
+  templateUrl: './lista-estudiantes-reliquidacion.component.html',
+  styleUrls: ['./lista-estudiantes-reliquidacion.component.scss']
+})
+export class ListaEstudiantesReliquidacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
