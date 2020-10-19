995964686b31a70154be845d5b88e2d8ae9482b6
Juanpa <jupmorenor@gmail.com>
2020-10-19 12:51:11 -0500

    limpieza de pruebas internas

--- src/app/pages/apoyo-alimentario/estudiantes/estudiantes.component.ts
+++ src/app/pages/apoyo-alimentario/estudiantes/estudiantes.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
 import { TranslateService } from '@ngx-translate/core';
 import { FormBuilder, Validators, FormControl, ValidatorFn } from '@angular/forms';
-import { EstudiantesService } from '../../../@core/helpers/estudiantes/estudiantes.service';
-import { Estudiante } from '../../../@core/data/models/estudiante';
 
 @Component({
   selector: 'ngx-estudiantes',
@@ -15,7 +13,6 @@ export class EstudiantesComponent implements OnInit {
 
   public resultado;
   estadoApoyo: string[] = ['Nuevo', 'Antiguo'];
-  estudiantes: Estudiante[] = [];
 
   solicitudAAFormulario = this.formBuilder.group({
     nombres: [''],
@@ -28,24 +25,19 @@ export class EstudiantesComponent implements OnInit {
   });
 
 
-  constructor(
-    private formBuilder: FormBuilder, 
-    private translate: TranslateService, 
-    private estudiantesService: EstudiantesService 
-    ) {
+  constructor(private formBuilder: FormBuilder, private translate: TranslateService, ) {
    }
 
   ngOnInit() {
-    this.consultarEstudiantes();
   }
 
-  guardar() {
-    // aca se envia la información a un servicio
-    console.log('quedo papu');
-    console.log(this.estudiantes);
-  }
+guardar() {
+  // aca se envia la información a un servicio
+  console.log('quedo papu');
+}
 
-  consultarEstudiantes() {
-    this.estudiantesService.getEstudiantesInfo().subscribe((data) => this.estudiantes = data.estudiantes.estudiante);
-  }
+
+
+
+
 }
