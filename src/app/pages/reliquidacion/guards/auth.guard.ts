1694f440d32ae5d994ef26c2bc221066eb127c4f
Camilo <camiloquirogam@outlook.com>
2020-10-19 18:37:09 -0500

    limpieza de codigo

--- src/app/pages/reliquidacion/guards/auth.guard.ts
+++ src/app/pages/reliquidacion/guards/auth.guard.ts
@@ -5,21 +5,20 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-<<<<<<< Updated upstream
-export class AuthGuard implements CanActivate {
-=======
 
 export class AuthGuard implements CanActivate {
 
->>>>>>> Stashed changes
+
+
+
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return true;
   }
 
-<<<<<<< Updated upstream
-}n
-=======
+
 }
->>>>>>> Stashed changes
+
+
