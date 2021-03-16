e282166a49832396d19093159268fe5d00a8dfc3
Sergio Gomez <laragsergio1803@gmail.com>
2021-03-16 18:00:17 -0500

    se agrego funcion de periodo para fechas

--- src/app/pages/apoyo-alimentario/administracion/fecha-crud/fecha-crud.component.ts
+++ src/app/pages/apoyo-alimentario/administracion/fecha-crud/fecha-crud.component.ts
@@ -12,6 +12,7 @@ import { IAppState } from '../../../../@core/store/app.state';
 import { ListService } from '../../../../@core/store/list.service';
 import { Periodo } from '../../../../@core/data/models/parametro/periodo';
 import { NbGlobalPhysicalPosition, NbToastrService } from '@nebular/theme';
+import { ParametroPeriodo } from '../../../../@core/data/models/parametro/parametro_periodo';
 
 @Component({
   selector: 'ngx-fecha-crud',
@@ -22,7 +23,6 @@ export class FechaCrudComponent implements OnInit {
 
   fecha = new FechaModel();
   fechas: FechaModel[]=[];
-  periodos: Periodo[] = [];
   periodo: Periodo = null;
 
   //periodoActual: PeriodoModel;
@@ -33,22 +33,83 @@ export class FechaCrudComponent implements OnInit {
     private store: Store<IAppState>,
     private listService: ListService,
     private toastrService: NbToastrService,) {
-    this.listService.findPeriodosAcademico();
-    this.loadLists();
+    this.listService.findServicioApoyo()
+    this.loadPeriodo();
   }
 
-  public loadLists() {
+  public loadPeriodo() {
     this.store.select((state) => state).subscribe(
       (list) => {
-        const listPA = list.listPeriodoAcademico
-        if (listPA.length > 0 && this.periodos.length === 0) {
-          const periodos = <Array<Periodo>>listPA[0]['Data'];
-          periodos.forEach(element => {
-            this.periodos.push(element);
-          })
+        const listaParam = list.listServicioApoyo;
+        if (listaParam.length > 0 && this.periodo === null) {
+          console.info("Periodo Fecha")
+          console.info(listaParam)
+          let parametros = <Array<ParametroPeriodo>>listaParam[0]['Data'];
+          this.periodo= parametros[0].PeriodoId
+          console.info(this.periodo)
         }
       },
-    );
+    );   
+  }
+
+  guardar(form:NgForm){
+    if(form.invalid){
+      Object.values(form.controls).forEach(control =>{
+        control.markAsTouched();
+      });
+      return;
+    }
+    
+    /* Swal.fire({
+      title: 'Espere',
+      text:'Guardando información',
+      icon: 'info',
+      allowOutsideClick: false,
+    });
+
+    Swal.showLoading();
+
+    let peticion: Observable<any>;
+
+    if(!this.fechasService.fechaActual(this.fecha.fechaDia)){
+      Swal.fire({
+        title: `Error en la Fecha: ${ this.fecha.fechaDia } `,
+        text: `Por favor agregue una fecha mayor o igual a la del dia actual `,
+        icon: 'error',
+        showConfirmButton: true,
+      });
+      return;
+    }
+    if(!this.fechaDiferente(this.fecha.fechaDia)){
+      Swal.fire({
+        title: `Error en la Fecha: ${ this.fecha.fechaDia } `,
+        text: `Ya existe una fecha para este Día`,
+        icon: 'error',
+        showConfirmButton: true,
+      });
+      return;
+    } 
+    if(this.fecha.id){
+      
+      peticion = this.fechasService.actualizar(this.fecha); 
+    }else{
+      
+      this.fecha.estado="pendiente";
+      peticion = this.fechasService.crearFecha(this.fecha);
+    }
+    
+
+    peticion.subscribe(resp => {
+      
+      Swal.fire({
+        title: this.fecha.fechaDia,
+        text: 'Se actualizo correctamente',
+        icon: 'success'
+      });
+      this.router.navigateByUrl('/fechas');
+
+    });  */
+    
   }
 
   ngOnInit() {
