e058a2b9a0ab98b4aeef85a618e464001265008a
Sergio Gomez <laragsergio1803@gmail.com>
2021-03-16 14:00:14 -0500

    Se actualizo la vista de fechas crud

--- src/app/pages/apoyo-alimentario/administracion/fecha-crud/fecha-crud.component.ts
+++ src/app/pages/apoyo-alimentario/administracion/fecha-crud/fecha-crud.component.ts
@@ -2,12 +2,16 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute,Router } from '@angular/router'
 import { pipe } from 'rxjs';
 import { FechaModel } from '../../../../@core/data/models/fecha/fecha.model';
-import { Periodo } from '../../../../@core/data/models/parametro/periodo';
 import { NgForm } from '@angular/forms';
 import Swal from 'sweetalert2';
 import { Observable } from 'rxjs';
 import { PeriodosService } from '../../servicios/periodos.service';
 import { FechasService } from '../../servicios/fechas.service'
+import { Store } from '@ngrx/store';
+import { IAppState } from '../../../../@core/store/app.state';
+import { ListService } from '../../../../@core/store/list.service';
+import { Periodo } from '../../../../@core/data/models/parametro/periodo';
+import { NbGlobalPhysicalPosition, NbToastrService } from '@nebular/theme';
 
 @Component({
   selector: 'ngx-fecha-crud',
@@ -18,11 +22,41 @@ export class FechaCrudComponent implements OnInit {
 
   fecha = new FechaModel();
   fechas: FechaModel[]=[];
+  periodos: Periodo[] = [];
+  periodo: Periodo = null;
+
   //periodoActual: PeriodoModel;
 
-  constructor() { }
+  constructor(private periodosService: PeriodosService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private store: Store<IAppState>,
+    private listService: ListService,
+    private toastrService: NbToastrService,) {
+    this.listService.findPeriodosAcademico();
+    this.loadLists();
+  }
+
+  public loadLists() {
+    this.store.select((state) => state).subscribe(
+      (list) => {
+        const listPA = list.listPeriodoAcademico
+        if (listPA.length > 0 && this.periodos.length === 0) {
+          const periodos = <Array<Periodo>>listPA[0]['Data'];
+          periodos.forEach(element => {
+            this.periodos.push(element);
+          })
+        }
+      },
+    );
+  }
 
   ngOnInit() {
+    
+  }
+
+  showToast() {
+    this.toastrService.show('Se registro correctamente', `Fecha Creada`,{position: NbGlobalPhysicalPosition.TOP_RIGHT , status: 'success' , duration: 2000 , icon: 'checkmark-square-outline'});
   }
 
 }
