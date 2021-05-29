4d39439f35edc25eb36ccd625eb88fb79d46de29
David <davidromeror98@gmail.com>
2021-05-29 13:17:25 -0500

    se agrega finalizacion y activacion masiva

--- src/app/pages/apoyo-alimentario/inscripciones/evaluacion-masiva/evaluacion-masiva.component.ts
+++ src/app/pages/apoyo-alimentario/inscripciones/evaluacion-masiva/evaluacion-masiva.component.ts
@@ -45,7 +45,7 @@ export class EvaluacionMasivaComponent implements OnInit {
   evaluacion = false;
   limite = -1;
   puntoInicial = 0;
-  solFinalizada= "activas";
+  solFinalizada = "activas";
 
   allSelect: boolean = false
 
@@ -123,20 +123,20 @@ export class EvaluacionMasivaComponent implements OnInit {
       this.utilService.showSwAlertError('Limite invalido', 'El limite puede ser -1 o un numero positivo');
       return;
     }
-    let finalizadas: boolean=null;
-    if(this.solFinalizada=='activas'){
-      finalizadas=false;
+    let finalizadas: boolean = null;
+    if (this.solFinalizada == 'activas') {
+      finalizadas = false;
     }
-    if(this.solFinalizada=='finalizadas'){
-      finalizadas=true;
+    if (this.solFinalizada == 'finalizadas') {
+      finalizadas = true;
     }
     console.log('Finalizadas: ', finalizadas);
-    
+
 
     this.solicitudesExt = []
     this.numSeleccionado = 0;
     this.allSelect = false;
-    
+
     this.listService.findSolicitudes(this.estadosTipoSolicitud[this.estadoTipo].Id, this.limite, this.puntoInicial, finalizadas).then((result) => {
       console.log(result);
       if (result.length > 0) {
@@ -157,11 +157,11 @@ export class EvaluacionMasivaComponent implements OnInit {
           }
           finally {
             if (countIter == result.length) {
-              if(this.solicitudesExt.length==0){
+              if (this.solicitudesExt.length == 0) {
                 let refSol: ReferenciaSolicitud = JSON.parse(result[0].Referencia);
-                this.utilService.showSwAlertError('No se encontraron solicitudes de '+this.periodos[this.periodo].Nombre,
-                        `No se encontraron solicitudes de este periodo, se encontraron solicitudes de ${refSol.Periodo}`)
-              }else{
+                this.utilService.showSwAlertError('No se encontraron solicitudes de ' + this.periodos[this.periodo].Nombre,
+                  `No se encontraron solicitudes de este periodo, se encontraron solicitudes de ${refSol.Periodo}`)
+              } else {
                 console.log(solicitud);
                 this.solicitudesExt.sort((a, b) => (b.Puntaje > a.Puntaje) ? 1 : ((a.Puntaje > b.Puntaje) ? -1 : 0));
               }
@@ -171,8 +171,8 @@ export class EvaluacionMasivaComponent implements OnInit {
 
         if (this.solicitudesExt.length == 0) {
           let refSol: ReferenciaSolicitud = JSON.parse(result[0].Referencia);
-          this.utilService.showSwAlertError('No se encontraron solicitudes de '+this.periodos[this.periodo].Nombre,
-                        `No se encontraron solicitudes de este periodo, se encontraron solicitudes de ${refSol.Periodo}`)
+          this.utilService.showSwAlertError('No se encontraron solicitudes de ' + this.periodos[this.periodo].Nombre,
+            `No se encontraron solicitudes de este periodo, se encontraron solicitudes de ${refSol.Periodo}`)
           /* this.utilService.showSwAlertError("Solicitudes no encontrados", `No se encontraron solicitudes ${this.estadosTipoSolicitud[this.estadoTipo].EstadoId.Nombre} para ${this.periodos[this.periodo].Nombre}`); */
         }
       } else {
@@ -299,18 +299,6 @@ export class EvaluacionMasivaComponent implements OnInit {
 
               }
             }
-            /*  for (let i = 0; i < this.solicitudes.length; i++) {
-               if (this.solicitudesExt[i].Seleccionado) {
-                 this.evaluarSolicitud(this.solicitudes[i], nuevoEstadoTipo, observacionObj).then((log) => {
-                   logEvents.push(log);
-                   actualizadas++;
-                   b.textContent = `${actualizadas}`;
-                   if (actualizadas == this.numSeleccionado) {
-                     Swal.close()
-                   }
-                 }).catch((logError) => logEvents.push('Error: ' + logError));
-               }
-             } */
           }).catch((err) => this.utilService.showSwAlertError("No se puede cargar tipo de observacion", err));
         } else {
           console.log("Se cancela");
@@ -343,6 +331,99 @@ export class EvaluacionMasivaComponent implements OnInit {
       });
     });
   }
+
+  cambiarSolicitudFinalizada(estado: boolean) {
+    /* if (this.solicitud.SolicitudFinalizada == estado) {
+        return;
+    } */
+    if (this.solFinalizada == 'todas') {
+      return;
+    }
+    if (estado && this.solFinalizada != 'activas') {
+      return;
+    }
+    if (!estado && this.solFinalizada != 'finalizadas') {
+      return;
+    }
+    let tituloObs = estado ? 'Finalizar' : 'Activar'
+    this.utilService.showSwAlertQuery(`¿Desea ${tituloObs} ${this.numSeleccionado} solicitudes?`,
+      `Vas a <b>${tituloObs}</b> las solicitudes de forma masiva`, tituloObs).then((respq) => {
+        if (respq) {
+          let logEvents: any[] = []
+          Swal.fire({
+            title: tituloObs + ' masiva de solicitudes',
+            html: `<b>0</b> de ${this.numSeleccionado} ... espere`,
+            timerProgressBar: true,
+            willOpen: () => {
+              Swal.showLoading();
+            },
+            willClose: () => {
+              let txtLogs = "";
+              for (const log of logEvents) {
+                txtLogs += log + "\n";
+              }
+              var blob = new Blob([txtLogs], { type: "text/plain;charset=utf-8" });
+              saveAs(blob, "registro de " + estado ? "finalizacion" : "activacion" + " masiva.txt");
+              this.utilService.showSwAlertSuccess(`${estado ? 'Finalizacion' : 'Activacion'} masiva`, `Se ${estado ? 'finalizaron' : 'activaron'} ${this.numSeleccionado} solicitudes de forma correcta`, "success");
+              this.solicitudesExt = []
+              this.numSeleccionado = 0;
+              this.allSelect = false;
+              this.evaluacion = false;
+            }
+          });
+          const content = Swal.getContent();
+          const b = content.querySelector('b');
+          let actualizadas = 0
+          logEvents.push("Logs");
+          this.listService.loadTiposObservacion(1).then((resp) => {
+            let observacionObj = new Observacion();
+            observacionObj.Titulo = estado ? 'Finalizacion' : 'Activacion' + " masiva";
+            observacionObj.Valor = `Accion realizada de forma masiva // ${this.utilService.getUsuarioWSO2()}`;
+            observacionObj.TipoObservacionId = resp['Data'][0];
+            for (const solicitudExt of this.solicitudesExt) {
+              if (solicitudExt.Seleccionado) {
+                this.cambiarEstado(solicitudExt.Solicitud, estado, observacionObj).then((log) => {
+                  logEvents.push(log);
+                  actualizadas++;
+                  b.textContent = `${actualizadas}`;
+                  if (actualizadas == this.numSeleccionado) {
+                    Swal.close()
+                  }
+                }).catch((logError) => logEvents.push('Error: ' + logError));
+              }
+            }
+
+          }).catch((err) => this.utilService.showSwAlertError("No se puede cargar tipo de observacion", err));
+        }
+      });
+  }
+
+  cambiarEstado(solicitud: Solicitud, finalizada: boolean, observacionObj: Observacion): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.listService.loadSolicitanteBySolicitud(solicitud.Id).then((respSolicitante) => {
+        let solicitante = respSolicitante;
+        if (solicitante != undefined) {
+          solicitud.SolicitudFinalizada = finalizada
+          this.listService.actualizarSolicitud(solicitud).then((resp) => {
+            observacionObj.SolicitudId = solicitud;
+            observacionObj.TerceroId = solicitante.TerceroId;
+            this.listService.crearObservacion(observacionObj).then((respObs) => {
+              resolve(`Se ${finalizada ? 'finalizo' : 'activo'} de forma correcta la solicitud #${solicitud.Id}`)
+            }).catch((errOb) => reject("No se creo la Observación para la solicitud #" + solicitud.Id));
+          }).catch((err) => {
+            reject(`No se pudo ${finalizada ? 'finalizar' : 'activar'} de forma correcta la solicitud #${solicitud.Id}`);
+          });
+        } else {
+          reject("No se cambio el estado para la solicitud #" + solicitud.Id);
+        }
+      }).catch((err) => {
+        reject("No se cambio el estado para la solicitud #" + solicitud.Id);
+      });
+    });
+  }
+
+
+
 
 }
 
