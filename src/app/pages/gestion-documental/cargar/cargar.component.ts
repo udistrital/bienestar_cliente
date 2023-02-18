9aba5e94e47d4c79601f6c5f78358c7ddd6c0f6c
nicolas240 <nicolasmadero24@gmail.com>
2023-02-17 20:08:51 -0500

    Co-authored-by: pragmatic-jeison-jara <pragmatic-jeison-jara@users.noreply.github.com>

--- src/app/pages/gestion-documental/cargar/cargar.component.ts
+++ src/app/pages/gestion-documental/cargar/cargar.component.ts
@@ -3,6 +3,11 @@ import { DocumentoG } from '../../../@core/data/models/documento/documento_Gesti
 import { NbDateService } from '@nebular/theme';
 import { NuxeoService } from '../../../@core/utils/nuxeo.service';
 import { DocumentoService } from '../../../@core/data/documento.service';
+import { Subject } from 'rxjs/Subject';
+import * as Nuxeo from 'nuxeo';
+import { GestionService } from '../gestion-documental.service';
+import { TipoDocumento } from '../../../@core/data/models/documento/tipo_documento';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'ngx-cargar',
@@ -14,49 +19,115 @@ export class CargarComponent implements OnInit {
   @ViewChild('labelImport',{static: false})
   labelImport: ElementRef;
 
-  
   // Estos datos se pueden traer de BD, para poder agregar mas
-  tiposDocumento: String [] = [
-    "Acta",
-    "Resolución",
-    "Comunicado",
-    "Contrato"
-  ];
-  documento: DocumentoG = new DocumentoG;
-  shortLink: string = "";
+  tiposDocumento: String [] = ["Acta", "Resolución", "Comunicado", "Contrato"];
+  
 
+  // Para cargar a nuxeo
+  documento: DocumentoG = new DocumentoG;
+  private blobDocument$ = new Subject<[object]>();
+  private blobDocument: object;
+  private nuxeo: Nuxeo;
+  
+  // Para formularios
+  private docForm: FormGroup;
+  private control: FormControl;
+  private validado: Boolean;
+  private clickeado: Boolean;
   
   //Rango de fechas de la carga
+  private min: Date;
+  private max: Date;
   
-  min: Date;
-  max: Date;
-  
-  constructor(
-    protected dateService: NbDateService<Date>,
-    private nuxeoService: NuxeoService,
-    private documentoService: DocumentoService
-    ) {
-    
+  constructor(protected dateService: NbDateService<Date>, private gestionService: GestionService, private fb: FormBuilder) {
+    gestionService= new GestionService;
+    this.documento.TipoDocumento = new TipoDocumento;
+    this.blobDocument = {};
+    this.validado = true;
+    this.clickeado= false;
     this.min = this.dateService.addYear(this.dateService.today(), -20);
     this.max = this.dateService.addMonth(this.dateService.today(), 1);
+    this.iniciarFormulario();
   }
 
-  ngOnInit() {
+  iniciarFormulario(){
+    this.docForm = this.fb.group({
+      tipoDocumento: ['', Validators.required],
+      titulo: ['', Validators.required] ,
+      serie: ['', Validators.required] ,
+      subSerie: ['', Validators.required],
+      fecha:['', [Validators.required, this.checkDate(this.min,this.max, this.dateService) ]],
+      descripcion: ['', Validators.required],
+      archivo: ['', Validators.required]
+    });
+    //Fecha por defecto
+    //this.docForm.get('fecha').setValue(this.dateService.format(this.dateService.today(),'dd/MM/yyyy'));
+    this.docForm.get('fecha').setValue(this.dateService.today());
+    //this.docForm.get('fecha').valid;
+    console.log('valid:' + this.docForm.get('fecha').valid);
+    this.docForm.controls.fecha.hasError('onRange');
+    
   }
-  cargarFormulario(){
-    this.nuxeoService.getDocumentos$([this.documento.Archivo],this.documentoService).subscribe;
-    //this.nuxeoService.getDocumentoById$([12],this.documentoService).subscribe();
-    console.log(this.documento.Archivo);
+
+  checkDate(min: Date, max: Date, date: NbDateService<Date>){
+    // Resta y suma un dia para que la validacion concuerde con el datapicker
+    // min = date.addDay(min, -1);
+    // max = date.addDay(max, 1);
+    console.log(this);
+    return (control: AbstractControl): { [key: string]: boolean } | null => {
+      if (control.value !== undefined && !(date.isBetween(new Date(control.value),min, max) )) {
+        return { 'onRange': true };
+      }
+      return null;
+    };
   }
+
+  ngOnInit() { }
   
-  // On file Select
+  cargarFormulario(){
+    // documento.TipoDocumento.Nombre se debe manejar por sistemas de OAS
+    if( this.docForm.invalid || this.docForm.controls.fecha.invalid){
+      this.validado= false;
+      return;
+    }
+    this.documento.TipoDocumento.Nombre=this.docForm.get('tipoDocumento').value;
+    this.documento.Nombre=this.docForm.get('titulo').value;
+    this.documento.Serie=this.docForm.get('serie').value;
+    this.documento.SubSerie=this.docForm.get('subSerie').value;
+    this.documento.Fecha=this.docForm.get('fecha').value;
+    this.documento.Descripcion=this.docForm.get('descripcion').value;
+    this.documento.Archivo=this.docForm.get('archivo').value;
+    console.log("carga realizandose...");
+    /* this.gestionService.crearFolder();
+    this.gestionService.crearDocumento(this.documento); */
+    console.log("carga realizada");
+
+    this.docForm.reset();
+    this.labelImport.nativeElement.innerText = 'Seleccione Archivo';
+    this.clickeado=false;
+    this.validado=true;
+  }
+
+  // On file Select change placeholder
   onChange(files: FileList) {
     //this.documento.Archivo = event.target.files[0];
     this.labelImport.nativeElement.innerText= Array.from(files)
       .map(f => f.name)
       .join(', ');
     this.documento.Archivo = files.item(0);
+  }
 
-}
-
+  //validación de los inputs con caso especial de la fecha implementando validacion checkDate
+  validateInput(input: string){
+    if(input === 'fecha'){
+      return (this.clickeado && this.docForm.controls[input].untouched) ||
+      (this.docForm.controls[input].invalid && this.docForm.controls[input].touched && this.docForm.controls.fecha.hasError('onRange'));
+    }else if(input === 'tipoDocumento'){
+      return (this.clickeado && this.docForm.controls[input].invalid && !this.docForm.controls[input].dirty);
+    }
+    else{
+      return (this.clickeado && this.docForm.controls[input].invalid && this.docForm.controls[input].untouched) ||
+      (this.docForm.controls[input].invalid && this.docForm.controls[input].touched);
+    } 
+  }
 }
---
+++ src/app/pages/gestion-documental/gestion-documental.service.ts
@@ -0,0 +1,98 @@
+import * as Nuxeo from 'nuxeo';
+import { Injectable } from '@angular/core';
+import { environment } from './../../../environments/environment';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import { Documento } from '../../shared/models/Salud/documento.model';
+import { NuxeoService } from '../../@core/utils/nuxeo.service';
+import { DocumentoG } from '../../@core/data/models/documento/documento_Gestion';
+@Injectable({
+    providedIn: 'root'
+  })
+export class GestionService {
+    static nuxeo: Nuxeo;
+
+    private documentos$ = new Subject<Documento[]>();
+    private documentos: object;
+
+
+    private blob: object;
+
+    private updateDoc$ = new Subject<[object]>();
+    private updateDoc: object
+
+    
+
+    constructor() {
+        this.documentos = {};
+        this.updateDoc = {};
+        
+        GestionService.nuxeo = new Nuxeo({
+            baseURL: environment.NUXEO.PATH,
+            auth: {
+                method: 'basic',
+                username: environment.NUXEO.CREDENTIALS.USERNAME,
+                password: environment.NUXEO.CREDENTIALS.PASS,
+            },
+        });
+    }
+
+    // Carga el documento a nux
+    crearDocumento(documento: DocumentoG){
+        const file = documento.Archivo;
+        console.log("creando documento");
+        GestionService.nuxeo.connect().
+            then(function(client){
+                GestionService.nuxeo.operation('Document.Create')
+                    .params({
+                        type: 'File',
+                        name: 'Documento de prueba',
+                        description: documento.Descripcion,
+                        properties: 'dc:title='+documento.Nombre,
+                        tag: documento.TipoDocumento.Nombre
+                        //properties: 'dc:title='+documento.Nombre+' \ndc:description=Documento para probar creacion'
+                    })
+                    .input('/'+'desarrollo'+'/'+'workspaces'+'/'+'pruebas'+'/'+'GestionDocumental')
+                    .execute()
+                    .then(function(res) {
+                        const blob = new Nuxeo.Blob({ content: file });
+                        console.log('Paso');
+                        console.log(blob);
+                        GestionService.nuxeo.batchUpload()
+                            .upload(blob)
+                            .then(function (response) {
+                                console.log(blob);
+                                GestionService.nuxeo.operation('Blob.AttachOnDocument')
+                                    .param('document',res.uid)
+                                    .input(response.blob)
+                                    .execute();
+                            }).catch(function (error) {
+                                console.log(error);
+                                return error;
+                            });
+                            console.log(blob);
+                    })
+            });
+
+        console.log(GestionService.nuxeo);
+    }
+
+    crearFolder(){
+        GestionService.nuxeo.connect()
+            .then(function (client) {
+                GestionService.nuxeo.operation('Document.Create')
+                .params({
+                    type: 'Folder',
+                    name: 'GestionDocumental',
+                    properties: 'dc:title=GestionDocumental \ndc:description=Folder de modulo gestion documental'
+                })
+                .input('/'+'desarrollo'+'/'+'workspaces'+'/'+'pruebas'+'/').execute()
+                .then(function(doc) {
+                    console.log('Created ' + doc.title + ' folder');
+                })
+        .catch(function(error) {
+          throw error;
+        });
+        });     
+    }
+}
