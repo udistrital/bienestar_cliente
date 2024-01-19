667cbddaa48493fc93fd61323e7f51418384a52e
Diego Andres Ferrer Tarazona <48162213+theglox@users.noreply.github.com>
2024-01-19 14:18:45 -0500

    Revert "Paz y salvos"

--- src/app/pages/acreditaciones-reportes/interfaces/atenciones.ts
+++
@@ -1,4 +0,0 @@
-export interface Atenciones {
-    nombre:string,
-    id:number
-}
--- src/app/pages/paz-y-salvos/pipes/fecha-format.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FechaFormatPipe } from './fecha-format.pipe';
-
-describe('FechaFormatPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FechaFormatPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
