2064829c76d75c3884d85e72ff7053e2186062d4
Luisa Cajamarca <lfcajamarcar@udistrital.edu.co>
2024-04-19 22:19:18 -0500

    carga y previsualizacion de evidencias basico

--- src/app/pages/cultura/actividad-cultural/cargar-evidencias/cargar-evidencias.component.ts
+++ src/app/pages/cultura/actividad-cultural/cargar-evidencias/cargar-evidencias.component.ts
@@ -17,8 +17,15 @@ import { Observable, ReplaySubject } from 'rxjs';
 export class CargarEvidenciasComponent implements OnInit {
   cargarEvidencias : FormGroup;
   public previsualizacion :string;
+  enlace : string;
   archivo :any;
   arch:File;
+  base64String: string;
+  imageSrc: any;
+  pdfSrc ="";
+  pdf:any;
+  
+
   base64:any = null;
   public archivos:any =[]
   tiposEvidencia: String[] = ['Imagenes', 'Enlaces de videos','Lista de asistencia'];
@@ -77,8 +84,9 @@ export class CargarEvidenciasComponent implements OnInit {
   cargarEvidencia(){
     
     const documento :Documento ={
-      IdTipoDocumento : 82,
-      nombre :"test4",
+      IdTipoDocumento : 85,
+      //nombre :"test19",
+      nombre :"test9",
       metadatos :{},
       descripcion:"Descripcion test",
       file : this.base64      
@@ -86,33 +94,28 @@ export class CargarEvidenciasComponent implements OnInit {
 
     let array = [documento];
     this.List.postDocumento(array).subscribe((data)=>{
+      this.enlace = data['res'].Enlace;
       console.log(data);
+      
     });
 
   }
 
-  extraerBase64 = async ($event:any) => new Promise((resolve,reject) => {
-    try {
-      const unsafeImg = window.URL.createObjectURL($event);
-      const image = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
-      const reader = new FileReader();
-      reader.readAsDataURL($event);
-      reader.onload =() => {
-        resolve({
-          base: reader.result
-        });
-      };
-      reader.onerror = error => {  
-        resolve({
-          base:null
-        });
-      };
-      
-    }catch(e){
-      return null;
-    }
+  obtenerFile(){
+    console.log(this.enlace);
 
-  });
+    this.List.getDocumento(this.enlace).subscribe((data2)=>{
+    this.base64String = data2['file'];
+    console.log(this.base64String);
+    //this.imageSrc = this.convertBase64ToImageSrc(this.base64String);
+    //this.pdf = this.convertBase64toFIle(this.base64String);
+    
+  
+    });
+  }
+
+
+ 
 
   convertFile(file: File): Observable<string> {
     const result = new ReplaySubject<string>(1);
@@ -122,7 +125,43 @@ export class CargarEvidenciasComponent implements OnInit {
     return result;
   }
 
-  
+  convertBase64ToImageSrc(base64String: string): any {
+    // Convertir la cadena base64 a una URL de objeto
+    const imageBlob = this.base64ToBlob(base64String, 'image/png');
+    const imageUrl = URL.createObjectURL(imageBlob);
+    // Sanitizar la URL para prevenir problemas de seguridad
+    return this.sanitizer.bypassSecurityTrustUrl(imageUrl);
+  }
+
+  printPdf() {    
+    const byteArray = new Uint8Array(
+      atob(this.base64String)
+        .split("")
+        .map(char => char.charCodeAt(0))
+    );
+    const file = new Blob([byteArray], { type: "application/pdf" });
+    const fileURL = URL.createObjectURL(file);
+    this.pdfSrc = fileURL;
+    console.log(this.pdfSrc);
+    window.open(fileURL);
+  }
+
+  base64ToBlob(base64String: string, type: string): Blob {
+    // Obtener el contenido binario de la cadena base64
+    const byteCharacters = atob(base64String);
+    const byteArrays = [];
+    for (let offset = 0; offset < byteCharacters.length; offset += 512) {
+      const slice = byteCharacters.slice(offset, offset + 512);
+      const byteNumbers = new Array(slice.length);
+      for (let i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+      const byteArray = new Uint8Array(byteNumbers);
+      byteArrays.push(byteArray);
+    }
+    // Crear un objeto Blob a partir de los byteArrays
+    return new Blob(byteArrays, { type: type });
+  }
 
 }
 
